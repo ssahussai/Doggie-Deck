edf519958a0512c19f72877389a99a4a07b7f2b0
Salma Khan <hussainsalma37@gmail.com>
2019-12-15 19:46:02 -0600

    adds dogs details page

--- README.md
+++ README.md
@@ -7,7 +7,7 @@ Salma Khan | December 15, 2019
 In this project I have created a modern full-stack reference app named DogCollector. 
 
 ## User Stories 
-* As a user (AAU), 
+* As a user (AAU), when I click the "View All My Dogs" link, I want to see a page listing all of my cats. 
 
 ## Technologies Used
 * Django
--- main_app/views.py
+++ main_app/views.py
@@ -1,6 +1,21 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 
+class Dog:
+    def __init__(self, name, breed, description, age):
+        self.name = name
+        self.breed = breed
+        self.description = description
+        self.age = age 
+
+dogs = [
+    Dog('Rufus', 'pomeranian', 'sweet little dog', 3),
+    Dog('Lia', 'poodle', 'full of energy and easy to train', 0),
+    Dog('Tommy', 'beagle', 'curious and very sweet', 4)
+]
+
+
+
 # Deine the home view
 def home(request):
     return HttpResponse('<h1>Hello Dog Lover!</h1>')
@@ -8,3 +23,7 @@ def home(request):
 # define about view
 def about(request):
     return render(request, 'about.html')
+
+# adds new view 
+def dogs_index(request):
+    return render(request, 'dogs/index.html', { 'dogs': dogs })
