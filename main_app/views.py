fcc4ec000685d48f0ca5a3deec320101c0320f4d
Salma Khan <hussainsalma37@gmail.com>
2019-12-15 21:47:11 -0600

    adds dog details functionality

--- main_app/views.py
+++ main_app/views.py
@@ -14,3 +14,7 @@ def about(request):
 def dogs_index(request):
     dogs = Dog.objects.all()
     return render(request, 'dogs/index.html', { 'dogs': dogs })
+
+def dogs_detail(request, dog_id):
+    dog = Dog.objects.get(id=dog_id)
+    return render(request, 'dogs/detail.html', { 'dog': dog })
