461433f795afaf706ac1701a55d889a236615712
Salma Khan <hussainsalma37@gmail.com>
2019-12-16 22:59:42 -0600

    Adding a Custom Method to Check the Feeding Status

--- main_app/models.py
+++ main_app/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.urls import reverse
+from datetime import date
 
 MEALS = (
     ('B', 'Breakfast'),
@@ -18,6 +19,9 @@ class Dog(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
+    
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
 
 class Feeding(models.Model):
     date = models.DateField('feeding date')
