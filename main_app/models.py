23d6ca51eb3a9027ef29d1031b5c66fb1baf349f
Salma Khan <hussainsalma37@gmail.com>
2019-12-16 21:51:35 -0600

    adds Feeding model

--- main_app/models.py
+++ main_app/models.py
@@ -1,7 +1,12 @@
 from django.db import models
 from django.urls import reverse
 
-# Create your models here.
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner'),
+)
+
 class Dog(models.Model):
     name = models.CharField(max_length=100)
     breed = models.CharField(max_length=100)
@@ -13,3 +18,16 @@ class Dog(models.Model):
     
     def get_absolute_url(self):
         return reverse('detail', kwargs={'dog_id': self.id})
+
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+
+    dog = models.ForeignKey(Dog, on_delete=models.CASCADE)
+    
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
