2e8121775166163313db6154910c17c288e67575
Salma Khan <hussainsalma37@gmail.com>
2019-12-16 22:45:32 -0600

    adds feeding schedules

--- main_app/models.py
+++ main_app/models.py
@@ -31,3 +31,6 @@ class Feeding(models.Model):
     
     def __str__(self):
         return f"{self.get_meal_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 from .models import Dog
 from .forms import FeedingForm
@@ -23,6 +23,16 @@ def dogs_detail(request, dog_id):
     return render(request, 'dogs/detail.html', { 
         'dog': dog, 'feeding_form': feeding_form 
     })
+
+def add_feeding(request, dog_id):
+    form = FeedingForm(request.POST)
+    if form.is_valid():
+        new_feeding = form.save(commit=False)
+        new_feeding.dog_id = dog_id
+        new_feeding.save()
+    return redirect('detail', dog_id=dog_id)
+
+
 
 class DogCreate(CreateView):
     model = Dog
