6f5756240bba5e0a2f009127f8363ced5ef56a73
Salma Khan <hussainsalma37@gmail.com>
2019-12-17 13:16:55 -0600

    implements CRUD on toy model

--- main_app/models.py
+++ main_app/models.py
@@ -16,7 +16,7 @@ class Toy(models.Model):
       return self.name
 
   def get_absolute_url(self):
-        return reverse('toys_detail', kwargs={'pk', self.id})
+      return reverse('toys_detail', kwargs={'pk': self.id})
    
 
 class Dog(models.Model):
